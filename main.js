ac1fde1f2f43fd212fd0b29dbc1d5b1d58189331
Park, Han Gyu <gominidive@hotmail.com>
2016-11-24 17:28:28 +0900

    Fixed Temperature & Added Ugly CSS

--- main.js
+++ main.js
@@ -1,28 +1,14 @@
 $(document).ready(function() {
   $('#get-data').click(function() {
-    var todayData = $('#today-data');
     var accessResult = $('#access-result');
 
     function extemp(temp, wspd) {
+      var wspd_kmh = 3.6 * wspd;
       return (13.12
         + 0.6215 * temp
         - 11.37  * Math.pow(wspd, 0.16)
-        + 0.3965 * Math.pow(wspd, 0.16) * temp);
+        + 0.3965 * Math.pow(wspd, 0.16) * temp).toFixed(2);
     }
-
-    /*
-    var latitude = -1;
-    var longitude = -1;
-
-    if(navigator.geolocation) {
-      var x = navigator.geolocation.getCurrentPosition(function(position) {
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
-      });
-    } else {
-      accessResult.text("Geolocation is not supported by this browser");
-    }
-    */
 
     // YESTERDAY
     $.ajax({
@@ -30,8 +16,8 @@ $(document).ready(function() {
       url:"http://apis.skplanetx.com/weather/history/yesterday",
       data: {
         version:"1",
-        lat:"37.5714000000",
-        lon:"126.9658000000",
+        lat:"37.4771",
+        lon:"126.9634",
         appKey:"0b5e02ae-9e17-36f8-9e88-c087f2b9b97b",
         isTimeRange:"Y"
       },
@@ -39,10 +25,13 @@ $(document).ready(function() {
       success: function(json) {
         //accessResult.text('Success');
         console.log(json);
-        $('#yesterday-sky').text(json.weather.yesterday[0].day.hourly[0].sky.name);
-        $('#yesterday-wspd').text(json.weather.yesterday[0].day.hourly[0].wind.wspd);
-        $('#yesterday-temp').text(json.weather.yesterday[0].day.hourly[0].temperature);
-        //$('#yesterday-extemp').text(extemp(parseFloat(temp), parseFloat(wspd)));
+        var sky = json.weather.yesterday[0].day.hourly[0].sky.name;
+        var wspd = json.weather.yesterday[0].day.hourly[0].wind.wspd;
+        var temp = json.weather.yesterday[0].day.hourly[0].temperature;
+        $('#yesterday-sky').text(sky);
+        $('#yesterday-wspd').text(wspd);
+        $('#yesterday-temp').text(temp);
+        $('#yesterday-extemp').text(extemp(parseFloat(temp), parseFloat(wspd)));
       },
       error: function(xhr, status, error) {
         //accessResult.text('Failed');
@@ -56,22 +45,26 @@ $(document).ready(function() {
       url:"http://apis.skplanetx.com/weather/current/hourly",
       data: {
         version:"1",
-        lat:"37.5714000000",
-        lon:"126.9658000000",
+        lat:"37.4771",
+        lon:"126.9634",
         appKey:"0b5e02ae-9e17-36f8-9e88-c087f2b9b97b"
       },
       dataType:"json",
       success: function(json) {
         //accessResult.text('Success');
         console.log(json);
-        $('#today-sky').text(json.weather.hourly[0].sky.name);
-        $('#today-temp').text(json.weather.hourly[0].temperature.tc);
+        var sky = json.weather.hourly[0].sky.name;
+        var wspd = json.weather.hourly[0].wind.wspd;
+        var temp = json.weather.hourly[0].temperature.tc;
+        $('#today-sky').text(sky);
+        $('#today-wspd').text(wspd);
+        $('#today-temp').text(temp);
+        $('#today-extemp').text(extemp(parseFloat(temp), parseFloat(wspd)));
       },
       error: function(xhr, status, error) {
         //accessResult.text('Failed');
         alert(error);
       }
     });
-    todayData.text('Hello, World!');
   });
 });
